01486f5f0574c9f69f18be9a1767dc783aa5115d
Suchita Doshi <sudoshi@linkedin.com>
2019-12-30 17:54:18 -0800

    add more test coverage

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -23,4 +23,76 @@ suite("Helper Test Suite", () => {
     assert.equal(helper.removePrefixSlash("app/foo/bar"), "app/foo/bar");
     assert.equal(helper.removePrefixSlash(), undefined);
   });
+
+  test("generateQuickPickItem - Returns correct label type", () => {
+    let fileName = "Users/projects/bang/app/foo/components/bar.js";
+    assert.equal(helper.generateQuickPickItem(fileName).label, "Component:");
+
+    fileName = "Users/projects/bang/app/foo/templates/components/bar.hbs";
+    assert.equal(helper.generateQuickPickItem(fileName).label, "Template:");
+
+    fileName = "Users/projects/bang/lib/bang/addon/routes/bar.js";
+    assert.equal(helper.generateQuickPickItem(fileName).label, "Route:");
+
+    fileName = "Users/projects/bang/app/styles/app.scss";
+    assert.equal(helper.generateQuickPickItem(fileName).label, "Style:");
+
+    fileName = "Users/projects/bang/tests/integration/bar-test.js";
+    assert.equal(
+      helper.generateQuickPickItem(fileName).label,
+      "Integration Test:"
+    );
+
+    fileName = "Users/projects/bang/tests/acceptance/bar-test.js";
+    assert.equal(
+      helper.generateQuickPickItem(fileName).label,
+      "Acceptance Test:"
+    );
+  });
+
+  test("getPrefixNameFromSpecialChars", () => {
+    assert.equal(
+      helper.getPrefixNameFromSpecialChars("_", null, "_bar"),
+      "bar"
+    );
+    assert.equal(
+      helper.getPrefixNameFromSpecialChars("_", "foo", "_foo-bar"),
+      "bar"
+    );
+    assert.equal(
+      helper.getPrefixNameFromSpecialChars("_", "foo", "bang"),
+      "bang"
+    );
+    assert.equal(helper.getPrefixNameFromSpecialChars(null, null, null), null);
+  });
+
+  test("getChildSubDir", () => {
+    const appSubDirRegex = `(\/.*)?/(app|addon)(\/.*)?\/(components|routes|templates)(\/.*)?\/(_)?foo.(js|hbs|scss|css)`;
+    const appTestSubDirRegex = `(\/.*)?/(tests)(\/.*)?\/(acceptance|unit)(\/(components|routes|templates))?(\/.*)?\/(_)?foo(-test)?.(js|hbs|scss|css)`;
+
+    let fileName = "Users/projects/app/components/admin/foo.js";
+    let appSubDirMatch = fileName.match(appSubDirRegex);
+    let testSubDirMatch = fileName.match(appTestSubDirRegex);
+    assert.equal(
+      helper.getChildSubDir(appSubDirMatch, testSubDirMatch),
+      "admin"
+    );
+
+    fileName =
+      "Users/projects/tests/acceptance/components/admin/settings/foo-test.js";
+    appSubDirMatch = fileName.match(appSubDirRegex);
+    testSubDirMatch = fileName.match(appTestSubDirRegex);
+    assert.equal(
+      helper.getChildSubDir(appSubDirMatch, testSubDirMatch),
+      "admin/settings"
+    );
+
+    fileName = "Users/projects/tests/integration/foo-test.js";
+    appSubDirMatch = fileName.match(appSubDirRegex);
+    testSubDirMatch = fileName.match(appTestSubDirRegex);
+    assert.equal(
+      helper.getChildSubDir(appSubDirMatch, testSubDirMatch),
+      undefined
+    );
+  });
 });
--- utils/helper.js
+++ utils/helper.js
@@ -1,8 +1,8 @@
 const vscode = require("vscode");
 const { Uri } = vscode;
 const {
-  createDefaultTestLabels,
-  createDefaultAppLabels
+  createDefaultAppLabelRuleSet,
+  createDefaultTestLabelRuleSet
 } = require("./labelsRuleset");
 const document =
   vscode.window.activeTextEditor && vscode.window.activeTextEditor.document;
@@ -22,6 +22,15 @@ function getPipedRegexString(...arr) {
 function removePrefixSlash(pathName) {
   return pathName && pathName[0] === "/" ? pathName.substr(1) : pathName;
 }
+
+/**
+ * Returns the sub directory path that comes after the app/test sub folders.
+ * Eg: for a given path: `Users/foo/app/components/bar/baz/bang.js`
+ * Here, appFolder -> components
+ * The function would return the sub directory of components, i.e. `bar/baz`
+ * @param {*} appRegexMatch
+ * @param {*} testRegexMatch
+ */
 function getChildSubDir(appRegexMatch, testRegexMatch) {
   if (appRegexMatch) {
     return removePrefixSlash(appRegexMatch[5]);
@@ -29,11 +38,18 @@ function getChildSubDir(appRegexMatch, testRegexMatch) {
   if (testRegexMatch) {
     return removePrefixSlash(testRegexMatch[7]);
   }
-  return null;
+  return;
 }
 
-// this is to match .scss files for patterns where css file names
-// are prefixed with either `_` or `_<folder-name>`
+/**
+ * This is to match the files for patterns where the file names
+ * are prefixed with any special character i.e. either `_` or `_<parent-folder-name>`
+ * Eg: `app/styles/_foo.scss` would be matched to `app/components/foo.js`
+ * OR `lib/bar/app/styles/_bar-foo.scss` would also be matched to `lib/bar/app/components/foo.js`
+ * @param {*} specialChar
+ * @param {*} potentialParentFolderName
+ * @param {*} prefix
+ */
 function getPrefixNameFromSpecialChars(
   specialChar,
   potentialParentFolderName,
@@ -42,13 +58,21 @@ function getPrefixNameFromSpecialChars(
   const prefixRegex = new RegExp(
     `(${specialChar})?(${potentialParentFolderName}-)?(.*)`
   );
-  const prefixRegexMatch = prefix.match(prefixRegex);
+  const prefixRegexMatch = prefix && prefix.match(prefixRegex);
   if (prefixRegexMatch) {
     prefix = prefixRegexMatch[prefixRegexMatch.length - 1];
   }
   return prefix;
 }
 
+/**
+ * Gather all the root and subroot folders from the config, and return the prefix
+ * of the path.
+ * Eg: if root and subroot folders is ['app', 'addon', 'tests'],
+ * item = `lib/bar/addon/components/foo.js`,
+ * it will return `lib/bar`.
+ * @param {*} item
+ */
 function _getPathPrefix(item) {
   const relativePath = vscode.workspace.asRelativePath(item);
   let pathPrefix = relativePath.split("/")[0];
@@ -72,12 +96,19 @@ function _getPathPrefix(item) {
   return pathPrefix;
 }
 
-function _getLabelForFile(metadataArray, appName, fileName) {
+/**
+ * Iterate over the array of the labelRuleSet metadata array and return the
+ * matched item's title.
+ * @param {*} metadataArray
+ * @param {*} prefixPath
+ * @param {*} fileName
+ */
+function _getLabelForFile(metadataArray, prefixPath, fileName) {
   for (let i = 0; i < metadataArray.length; i++) {
     const item = metadataArray[i];
     if (item.regexPatterns && item.regexPatterns.length) {
       for (let j = 0; j < item.regexPatterns.length; j++) {
-        const regexItem = `${appName}${item.regexPatterns[j]}`;
+        const regexItem = `${prefixPath}${item.regexPatterns[j]}`;
         if (fileName.match(regexItem)) {
           return `${item.title}:`;
         }
@@ -87,20 +118,31 @@ function _getLabelForFile(metadataArray, appName, fileName) {
   return;
 }
 
-function determineLabelType(fileName, appName) {
+/**
+ * Returns a matched label for a given fileName and a prefix path based on
+ * a combination of user's provided custom label rule set, default app rule set
+ * and test rule set.
+ * @param {*} fileName
+ * @param {*} prefixPath
+ */
+function determineLabelType(fileName, prefixPath) {
   const userDefinedLabels = config.labelRuleSets || [];
-  const testLabels = createDefaultTestLabels(config.testSubFolders);
-  const appLabels = createDefaultAppLabels(config.appSubFolders);
+  const testLabelRuleSet = createDefaultTestLabelRuleSet(config.testSubFolders);
+  const appLabelRuleSet = createDefaultAppLabelRuleSet(config.appSubFolders);
 
   return (
     _getLabelForFile(
-      [].concat(...userDefinedLabels, ...testLabels, ...appLabels),
-      appName,
+      [].concat(...userDefinedLabels, ...testLabelRuleSet, ...appLabelRuleSet),
+      prefixPath,
       fileName
     ) || ""
   );
 }
 
+/**
+ * Returns the quick pick item object based on the file path.
+ * @param {*} item
+ */
 function generateQuickPickItem(item) {
   const maybeWorkspaceFolder = workspaceFolder ? workspaceFolder.uri.path : "";
   const pathPrefix = _getPathPrefix(item);
